4566b0536a658d001921db42de29840e90279cf7
marcoambricca <ambriccam@gmail.com>
2026-04-21 17:27:38 -0300

    var de entorno dns arreglada

--- src/lib/api.js
+++ src/lib/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
 const api = axios.create({
-  baseURL: `${API_URL}api`,
+  baseURL: `${API_URL}/api`,
   withCredentials: true,
 });
 
